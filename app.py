eb2b3810627132e4b3d6e87af04219a51907347c
ashleigh <ashleighdevito@gmail.com>
2021-10-22 13:12:39 -0400

    formatting predictions

--- app.py
+++ app.py
@@ -54,54 +54,78 @@ def model_page():
 # Set route
 @app.route("/predict-dune")
 def predict_dune():
-    #float_features = [float(x) for x in request.form.values()]
+    # float_features = [float(x) for x in request.form.values()]
     # float_features = [128,53,5.6,400000,6,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,11,0,0,0,1,0,0,0,0,0,0]
     # features = [np.array(float_features)]
     
-    prediction = model.predict(dune)
-    return render_template("index.html", prediction_text = "Dune has a boxoffice prediction of {}".format(prediction))
+    diction = str(model.predict(dune))
+    chunks = diction.split("'")
+    prediction = chunks[1]
+
+    return render_template("index.html", prediction_text = "Dune has a boxoffice prediction of {}.".format(prediction))
 
 @app.route("/predict-ron")
 def predict_ron():
     
-    prediction = model.predict(ron)
-    return render_template("index.html", prediction_text = "Ron's Gone Wrong has a boxoffice prediction of {}".format(prediction))
+    diction = str(model.predict(ron))
+    chunks = diction.split("'")
+    prediction = chunks[1]
+
+    return render_template("index.html", prediction_text = "Ron's Gone Wrong has a boxoffice prediction of {}.".format(prediction))
 
 @app.route("/predict-souvenir")
 def predict_souvenir():
-    
-    prediction = model.predict(souvenir)
-    return render_template("index.html", prediction_text = "The Souvenir: Part II has a boxoffice prediction of {}".format(prediction))
+
+    diction = str(model.predict(souvenir))
+    chunks = diction.split("'")
+    prediction = chunks[1]
+
+    return render_template("index.html", prediction_text = "The Souvenir: Part II has a boxoffice prediction of {}.".format(prediction))
 
 @app.route("/predict-spencer")
 def predict_spencer():
-    
-    prediction = model.predict(spencer)
-    return render_template("index.html", prediction_text = "Spencer has a boxoffice prediction of {}".format(prediction))
+
+    diction = str(model.predict(spencer))
+    chunks = diction.split("'")
+    prediction = chunks[1]
+
+    return render_template("index.html", prediction_text = "Spencer has a boxoffice prediction of {}.".format(prediction))
 
 @app.route("/predict-mothering_sunday")
 def predict_mothering():
-    
-    prediction = model.predict(mothering_sunday)
-    return render_template("index.html", prediction_text = "Mothering Sunday has a boxoffice prediction of {}".format(prediction))
+
+    diction = str(model.predict(mothering_sunday))
+    chunks = diction.split("'")
+    prediction = chunks[1]
+
+    return render_template("index.html", prediction_text = "Mothering Sunday has a boxoffice prediction of {}.".format(prediction))
 
 @app.route("/predict-humans")
 def predict_humans():
-    
-    prediction = model.predict(humans)
-    return render_template("index.html", prediction_text = "The Humans has a boxoffice prediction of {}".format(prediction))
+
+    diction = str(model.predict(humans))
+    chunks = diction.split("'")
+    prediction = chunks[1]
+
+    return render_template("index.html", prediction_text = "The Humans has a boxoffice prediction of {}.".format(prediction))
 
 @app.route("/predict-rocket")
 def predict_rocket():
-    
-    prediction = model.predict(rocket)
-    return render_template("index.html", prediction_text = "Red Rocket has a boxoffice prediction of {}".format(prediction))
+
+    diction = str(model.predict(rocket))
+    chunks = diction.split("'")
+    prediction = chunks[1]
+
+    return render_template("index.html", prediction_text = "Red Rocket has a boxoffice prediction of {}.".format(prediction))
 
 @app.route("/predict-song")
 def predict_song():
-    
-    prediction = model.predict(song)
-    return render_template("index.html", prediction_text = "Swan Song has a boxoffice prediction of {}".format(prediction))
+
+    diction = str(model.predict(song))
+    chunks = diction.split("'")
+    prediction = chunks[1]
+
+    return render_template("index.html", prediction_text = "Swan Song has a boxoffice prediction of {}.".format(prediction))
 
 if __name__ == '__main__':
     app.jinja_env.cache = {}
