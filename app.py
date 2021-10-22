5078a70aed01b88ece56ba407c204836601789b4
ashleigh <ashleighdevito@gmail.com>
2021-10-22 11:39:34 -0400

    Flask app functional, webpage up

--- app.py
+++ app.py
@@ -26,19 +26,15 @@ model = pickle.load(open("rf_model.pkl", "rb"))
 # Create an instance of our Flask app.
 app = Flask(__name__)
 
-
-
 @app.route("/")
 def home():
 
-
     # Return template and data
-    return render_template("index.html")
+    return render_template("index.html", prediction_text = "Run the model to see a prediction.")
 
 
 @app.route("/index")
 def index():
-
 
     # Return template and data
     return render_template("index.html")
@@ -46,10 +42,14 @@ def index():
 @app.route("/team")
 def team():
 
-
     # Return template and data
     return render_template("team.html")
+    
+@app.route("/model")
+def model_page():
 
+    # Return template and data
+    return render_template("model.html")
 
 # Set route
 @app.route("/predict-dune")
@@ -59,44 +59,43 @@ def predict_dune():
     # features = [np.array(float_features)]
     
     prediction = model.predict(dune)
-    return render_template("index.html", prediction_text = "This film has a boxoffice prediction of {}".format(prediction))
+    return render_template("index.html", prediction_text = "Dune has a boxoffice prediction of {}".format(prediction))
 
 @app.route("/predict-ron")
 def predict_ron():
     
-    
     prediction = model.predict(ron)
-    return render_template("index.html", prediction_text = "This film has a boxoffice prediction of {}".format(prediction))
+    return render_template("index.html", prediction_text = "Ron's Gone Wrong has a boxoffice prediction of {}".format(prediction))
 
 @app.route("/predict-souvenir")
 def predict_souvenir():
     
     prediction = model.predict(souvenir)
-    return render_template("index.html", prediction_text = "This film has a boxoffice prediction of {}".format(prediction))
+    return render_template("index.html", prediction_text = "The Souvenir: Part II has a boxoffice prediction of {}".format(prediction))
 
 @app.route("/predict-spencer")
 def predict_spencer():
     
     prediction = model.predict(spencer)
-    return render_template("index.html", prediction_text = "This film has a boxoffice prediction of {}".format(prediction))
+    return render_template("index.html", prediction_text = "Spencer has a boxoffice prediction of {}".format(prediction))
 
 @app.route("/predict-mothering_sunday")
 def predict_mothering():
     
     prediction = model.predict(mothering_sunday)
-    return render_template("index.html", prediction_text = "This film has a boxoffice prediction of {}".format(prediction))
+    return render_template("index.html", prediction_text = "Mothering Sunday has a boxoffice prediction of {}".format(prediction))
 
 @app.route("/predict-humans")
 def predict_humans():
     
     prediction = model.predict(humans)
-    return render_template("index.html", prediction_text = "This film has a boxoffice prediction of {}".format(prediction))
+    return render_template("index.html", prediction_text = "The Humans has a boxoffice prediction of {}".format(prediction))
 
 @app.route("/predict-rocket")
 def predict_rocket():
     
     prediction = model.predict(rocket)
-    return render_template("index.html", prediction_text = "This film has a boxoffice prediction of {}".format(prediction))
+    return render_template("index.html", prediction_text = "Red Rocket has a boxoffice prediction of {}".format(prediction))
 
 if __name__ == '__main__':
     app.jinja_env.cache = {}
